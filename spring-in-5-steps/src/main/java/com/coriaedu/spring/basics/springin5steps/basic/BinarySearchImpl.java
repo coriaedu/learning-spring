1938ed6e59b7ae1afc2d4742b7d3ea9b36be4029
Eduardo Coria <emcoria@us.ibm.com>
2018-07-23 10:12:20 -0700

    26. Step 17 - Lifecycle of a Bean - @PostConstruct and @PreDestroy

--- spring-in-5-steps/src/main/java/com/coriaedu/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/coriaedu/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.coriaedu.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -10,14 +15,16 @@ import org.springframework.stereotype.Component;
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
     @Qualifier("bubble")
-    private SortAlgorithm quickSortAlgorithm;
+    private SortAlgorithm sortAlgorithm;
 
     public int binarySearch(int[] numbers, int numberToSearchFor) {
 		// Sorting an array
-        System.out.println(quickSortAlgorithm);
-        int[] sorted = quickSortAlgorithm.sort(numbers);
+        System.out.println(sortAlgorithm);
+        int[] sorted = sortAlgorithm.sort(numbers);
 
 
 		// Search the array
@@ -25,4 +32,15 @@ public class BinarySearchImpl {
 		// Return the index of the result
 		return 3;
 	}
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("Post Construct");
+        logger.info("SortAlgorithm: {}", sortAlgorithm);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("Pre Destroy");
+    }
 }
