8febf52c58badc4fa3bcacc9277a62ebf1237222
Eduardo Coria <emcoria@us.ibm.com>
2018-07-24 10:25:50 -0700

    31-32. Steps 21-22 - Defining Spring Application Context using XML

---
+++ spring-in-5-steps/src/main/java/com/coriaedu/spring/basics/springin5steps/SpringIn5StepsXmlContextApplication.java
@@ -0,0 +1,39 @@
+package com.coriaedu.spring.basics.springin5steps;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.coriaedu.spring.basics.springin5steps.xml.XmlPersonDAO;
+
+@Configuration
+@ComponentScan("") // current package
+public class SpringIn5StepsXmlContextApplication {
+
+    // What are the beans?
+    // Through the @Component annotation, we tell Spring which are the beans we want it to manage.
+    // If we are not using Spring Boot, we can use just Spring and use the @Configuration and @ComponentScan annotations.
+
+    // What are the dependencies of a bean?
+    // We use @Autowired on the dependencies within a bean.
+
+    // Before Spring 2.5, beans, dependencies and configuration was done through XML files loaded from the classpath
+
+    // Where to search for beans?
+    // Spring Boot will scan the component packages through the @SpringBootApplication annotated class' package and sub packages.
+    // If using just Spring, @ComponentScan("") is used.
+
+    public static void main(String[] args) {
+
+        // Spring Application Context will manage all the beans:
+        try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "applicationContext.xml")) { // no need to provide path as this is in the Classpath (main/resources)
+
+            XmlPersonDAO person = applicationContext.getBean(XmlPersonDAO.class);
+
+            System.out.println(person);
+            System.out.println(person.getXmlJdbcConnection());
+
+        }
+    }
+}
