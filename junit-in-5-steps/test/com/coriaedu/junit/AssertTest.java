af171fe831e87a3ff70137a3c2da26426888cfcd
Eduardo Coria <emcoria@us.ibm.com>
2018-07-25 15:51:22 -0700

    42. Step5: Important annotations

--- junit-in-5-steps/test/com/coriaedu/junit/AssertTest.java
+++ junit-in-5-steps/test/com/coriaedu/junit/AssertTest.java
@@ -1,10 +1,10 @@
 package com.coriaedu.junit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class AssertTest {
 
--- junit-in-5-steps/test/com/coriaedu/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/coriaedu/junit/MyMathTest.java
@@ -2,19 +2,45 @@ package com.coriaedu.junit;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-class MyMathTest {
+public class MyMathTest {
     MyMath myMath = new MyMath();
+
+    @Before
+    public void before() {
+        System.out.println("@Before");
+    }
+
+    @After
+    public void after() {
+        System.out.println("@After");
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("@BeforeClass");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("@AfterClass");
+    }
 
     @Test
     public void sum_with3numbers() {
+        System.out.println("Test1");
         // check that result == 6
         assertEquals(6, myMath.sum(new int[] { 1, 2, 3 }));
     }
 
     @Test
     public void sum_with1number() {
+        System.out.println("Test2");
         // check that result == 3
         assertEquals(3, myMath.sum(new int[] { 3 }));
     }
