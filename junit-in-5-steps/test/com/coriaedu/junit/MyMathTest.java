92b211935a319b5759da7769ef1dcaa54102afd6
Eduardo Coria <emcoria@us.ibm.com>
2018-07-25 15:15:28 -0700

    40. Step 3 : First Code and First Unit Test

--- junit-in-5-steps/test/com/coriaedu/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/coriaedu/junit/MyMathTest.java
@@ -1,12 +1,22 @@
 package com.coriaedu.junit;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
+    MyMath myMath = new MyMath();
 
     @Test
-    void test() {
-        // Absence of failure is success
+    public void sum_with3numbers() {
+        // check that result == 6
+        assertEquals(6, myMath.sum(new int[] { 1, 2, 3 }));
+    }
+
+    @Test
+    public void sum_with1number() {
+        // check that result == 3
+        assertEquals(3, myMath.sum(new int[] { 3 }));
     }
 
 }
