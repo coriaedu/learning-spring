9ee250e3fcd4f05790831613af43a4e1932b7000
Eduardo Coria <emcoria@us.ibm.com>
2018-08-06 15:52:13 -0700

    58. Step 5: What is Spring Boot Auto Configuration?

--- springboot-in-10-steps/src/main/java/com/coriaedu/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/coriaedu/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,11 +2,17 @@ package com.coriaedu.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class,
+                args);
+
+        for (String beanName : applicationContext.getBeanDefinitionNames()) {
+            System.out.println("BEAN: " + beanName);
+        }
 	}
 }
