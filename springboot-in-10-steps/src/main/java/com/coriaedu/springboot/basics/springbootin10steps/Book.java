a4742cfe6675281bc83781e80e7227dcc6cecc56
Eduardo Coria <emcoria@us.ibm.com>
2018-08-06 14:39:53 -0700

    57. Step 4: Creating a Simple REST Controller

---
+++ springboot-in-10-steps/src/main/java/com/coriaedu/springboot/basics/springbootin10steps/Book.java
@@ -0,0 +1,32 @@
+package com.coriaedu.springboot.basics.springbootin10steps;
+
+public class Book {
+
+    long id;
+    String name;
+    String author;
+
+    public Book(long id, String title, String author) {
+        super();
+        this.id = id;
+        this.name = title;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book [id=%s, name=%s, author=%s]", id, name, author);
+    }
+}
