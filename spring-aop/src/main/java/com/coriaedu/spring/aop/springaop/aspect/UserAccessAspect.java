cf337b447c17eb8398977b85a9a3186a5ec4493a
Eduardo Coria <emcoria@us.ibm.com>
2018-08-13 16:04:47 -0700

    70. Step 04: Undertand AOP Terminology: Pointcut, Advice, Aspect and Join Point

--- spring-aop/src/main/java/com/coriaedu/spring/aop/springaop/aspect/UserAccessAspect.java
+++ spring-aop/src/main/java/com/coriaedu/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-@Aspect // AOP
+@Aspect // The combination of a Pointcut and an Advice
 @Configuration // Configuration
 public class UserAccessAspect {
 
@@ -15,10 +15,11 @@ public class UserAccessAspect {
 
     // Define what kind of method calls I would like to intercept
     // execution( * PACKAGE.*.*(..))
-
-    @Before("execution(* com.coriaedu.spring.aop.springaop.business.*.*(..))")
-    public void before(JoinPoint joinPoint) {
-        // What to do
+    // Weaving: Process where this whole things get weaved around our code is called Weaving.
+    // Weaver: Framework that does weaving.
+    @Before("execution(* com.coriaedu.spring.aop.springaop.data.*.*(..))") // This expression is called a Pointcut (what we want to intercept)
+    public void before(JoinPoint joinPoint) { // A JoinPoint is specific interception of a method call.
+        // What to do: This is called the Advice
         log.info("Check for user access");
         log.info("Allowed execution for {}", joinPoint);
     }
