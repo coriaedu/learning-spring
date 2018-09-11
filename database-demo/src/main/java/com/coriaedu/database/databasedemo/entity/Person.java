8f3fc02ccaef7e5c38985e3a5df782f8780ba177
Eduardo Coria <emcoria@us.ibm.com>
2018-09-11 14:17:04 -0700

    90. Step 14 - Defining Person Entity

--- database-demo/src/main/java/com/coriaedu/database/databasedemo/entity/Person.java
+++ database-demo/src/main/java/com/coriaedu/database/databasedemo/entity/Person.java
@@ -2,9 +2,21 @@ package com.coriaedu.database.databasedemo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person") // Not really needed as the class name already matches the table name.
 public class Person {
 
+    @Id // Indicates this is a primary key
+    @GeneratedValue // Uses the database mechanism to generate the id.
     private int id;
+
+    @Column(name = "name") // Not really needed as the field name already matches the column name
     private String name;
     private String location;
     private Date birthDate;
@@ -16,6 +28,14 @@ public class Person {
     public Person(int id, String name, String location, Date birthDate) {
         super();
         this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public Person(String name, String location, Date birthDate) {
+        super();
+        //        this.id = id; // This constructor is for hibernate - It does not use the id.
         this.name = name;
         this.location = location;
         this.birthDate = birthDate;
