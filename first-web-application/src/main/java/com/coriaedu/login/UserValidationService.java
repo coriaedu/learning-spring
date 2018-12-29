be119597a6f2f4c6540058200f6daae7682e7eb2
Eduardo Coria <emcoria@us.ibm.com>
2018-12-28 18:29:42 -0800

    119. Step 17 : Spring Autowiring and Dependency Injection

--- first-web-application/src/main/java/com/coriaedu/login/UserValidationService.java
+++ first-web-application/src/main/java/com/coriaedu/login/UserValidationService.java
@@ -1,5 +1,8 @@
 package com.coriaedu.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserValidationService {
 
     public boolean isUserValid(String username, String password) {
